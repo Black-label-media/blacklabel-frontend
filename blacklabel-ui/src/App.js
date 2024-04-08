917c816b8acf81d2293296ec6ed5b6fd21de6cbd
murdanetaos <miguelurd10@gmail.com>
2024-04-07 20:19:25 -0400

    hice cambios menores

--- blacklabel-ui/src/App.js
+++ blacklabel-ui/src/App.js
@@ -1,14 +1,11 @@
-
 import './App.css';
-import HelloWorld from './components/HelloWorld';
-import './components/HelloStyles.css'
+
 
 
 
 function App() {
   return (
     <div className="App">
-      <HelloWorld/>
     </div>
   );
 }
